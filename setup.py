c1a37831a28c681ccc3ec886ce2d9642d5d6e3b2
SpaceMan <155488906+SpacemanMy@users.noreply.github.com>
2025-12-31 22:41:06 +0000

    Add setup.py for project configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+# pyproject.toml (modern way)
+[project]
+name = "ternarycore"
+version = "0.1"
+dependencies = []
+
+[build-system]
+requires = ["setuptools"]
+build-backend = "setuptools.build_meta"
